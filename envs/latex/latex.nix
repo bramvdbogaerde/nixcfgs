d5d487cb0ca34a86194134c742b183bf6cd0e957
bramvdbogaerde <contact@bramvdb.com>
2021-01-11 14:04:05 +0100

    updated latex config

--- envs/latex/latex.nix
+++ envs/latex/latex.nix
@@ -1,14 +1,17 @@
 { pkgs ? import <nixpkgs> {}, 
   latex ? pkgs.texlive.combined.scheme-full,
+  pygments ? pkgs.python38Packages.pygments,
   additionalInputs ? [],
   ...}@attrs: 
 
 let rest = builtins.removeAttrs attrs [
    "pkgs"
    "latex"
+   "pygments"
 ]; in 
 pkgs.mkShell ({
   buildInputs = [
     latex
+    pygments
   ] ++ additionalInputs;
 } // rest)
