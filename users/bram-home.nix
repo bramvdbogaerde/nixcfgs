c1480228fd247c221d173fad1ff9f8d55b4b12c0
bramvdbogaerde <contact@bramvdb.com>
2021-01-11 14:03:57 +0100

    updated coc config to jump to next error with shortcut

--- users/bram-home.nix
+++ users/bram-home.nix
@@ -116,6 +116,8 @@ let nixpkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/mast
         colorscheme gruvbox
         let g:airline_poweraline_fonts = 1
         map <C-d> :NERDTreeToggle<CR>
+        nmap <silent> <leader>en <Plug>(coc-diagnostic-next-error)
+
         let g:ctrlp_custom_ignore = 'vendor\|pkg\|node_modules\|build\|target'
         let g:ctrlp_use_caching = 0
 
